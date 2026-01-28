60a0fc36e113d079cbda94ddf5e5f3ab2de83246
“Ayush” <ayushyadavkr0@tuta.io>
2026-01-28 15:13:41 +0530

    refactor: format export statements for ParticipantList and related types

--- components/splits/index.ts
+++ components/splits/index.ts
@@ -1,4 +1,8 @@
 export { ContactSelect } from "./contact-select";
 export { SplitConfiguration } from "./split-configuration";
 export { SplitExpenseDialog } from "./split-expense-dialog";
-export { ParticipantList, type ParticipantSplit, type SplitMethod } from "./participant-list";
+export {
+  ParticipantList,
+  type ParticipantSplit,
+  type SplitMethod,
+} from "./participant-list";
